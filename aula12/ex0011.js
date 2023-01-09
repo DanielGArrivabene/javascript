94a07bfcbcb919186846f0b85804e6d9bf950e55
Daniel Goulart Arrivabene <119176943+DanielGArrivabene@users.noreply.github.com>
2023-01-09 11:47:07 -0300

    ok

---
+++ aula12/ex0011.js
@@ -0,0 +1,9 @@
+var idade = 65
+console.log('VOCÊ TEM ' + idade + ' ANOS')
+if (idade < 16) {
+    console.log('NÃO VOTA')
+} else if (idade < 18 || idade >= 65) {
+    console.log('VOTO OPCIONAL')
+} else {
+    console.log('VOTO OBRIGATÓRIO')
+}
